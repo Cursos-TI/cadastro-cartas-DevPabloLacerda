f16139492ce6a020ecf6d0bc9bf8d7ccdc765bf5
DevPabloLacerda <dev.pablolacerda@gmail.com>
2026-01-13 07:14:27 +0000

    Adicionei a batalha entre as cartas para ver qual é a mais forte Adicionei a variável SuperPoder e a batalha foi realizar com Modificadores relacionais e modifiquei a várivel int para unsigned

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,13 +4,14 @@ int main(){
     
     int Número_de_Pontos_Turísticos1; //Numero inteiro
     int Número_de_Pontos_Turísticos2; //Separando em carta 1 e 2
-    float População1, População2;
+    unsigned long int População1, População2;
     float Área1, PIB1, Área2, PIB2; //ponto flutuante no formato padrão
     char Estado1, Estado2; //apenas uma letra
     char Código1[10], Código2[10]; //conjunto de caracteres 
     char Nome_da_Cidade1[15], Nome_da_Cidade2[15]; //conjunto de caracteres 
     float Densidade1, Densidade2;
     float PerCapita1, PerCapita2;
+    float SuperPoder1, SuperPoder2;
 
 
 
@@ -26,7 +27,7 @@ int main(){
     scanf ("%s", Nome_da_Cidade1);
 
     printf ("População: ");
-    scanf ("%f", &População1);
+    scanf ("%u", &População1);
 
     printf ("Área: ");
     scanf ("%f", &Área1);
@@ -40,6 +41,8 @@ int main(){
     Densidade1 = População1 / Área1;
    
     PerCapita1 = PIB1 / População1;
+
+    SuperPoder1 = População1 + Área1 + PIB1 + Número_de_Pontos_Turísticos1 + PerCapita1;
   
     //carta 2
 
@@ -53,7 +56,7 @@ int main(){
     scanf ("%s", Nome_da_Cidade2);
 
     printf ("População: ");
-    scanf ("%f", &População2);
+    scanf ("%u", &População2);
 
     printf ("Área: ");
     scanf ("%f", &Área2);
@@ -68,6 +71,7 @@ int main(){
     
     PerCapita2 = PIB2 / População2;
 
+    SuperPoder2 = População2 + Área2 + PIB2 + Número_de_Pontos_Turísticos2 + PerCapita2;
    
     //exibindo resutado das duas cartas abaixo
 
@@ -75,12 +79,13 @@ int main(){
     printf ("Estado: %c \n", Estado1);
     printf ("Código: %s \n", Código1);
     printf ("Nome da Cidade: %s\n", Nome_da_Cidade1);
-    printf ("População: %f \n", População1);
+    printf ("População: %u \n", População1);
     printf ("Área: %.2f km² \n", Área1);
     printf ("PIB: %.2f bilhões de reais\n", PIB1);
     printf ("Número de Pontos Turísticos: %d\n", Número_de_Pontos_Turísticos1);
     printf ("Densidade Populacional: %f hab/km²\n", Densidade1);
     printf ("PIB per Capita: %f reais\n", PerCapita1);
+    printf ("Super Poder: %f\n", SuperPoder1);
     
     //info carta 2
 
@@ -88,12 +93,24 @@ int main(){
     printf ("Estado: %c \n", Estado2);
     printf ("Código: %s \n", Código2);
     printf ("Nome da Cidade: %s \n", Nome_da_Cidade2);
-    printf ("População: %f \n", População2);
+    printf ("População: %u \n", População2);
     printf ("Área: %.2f km² \n", Área2);
     printf ("PIB: %.2f bilhões de reais\n", PIB2);
     printf ("Número de Pontos Turísticos: %d\n", Número_de_Pontos_Turísticos2);
     printf ("Densidade Populacional: %f hab/km²\n", Densidade2);
     printf ("PIB per Capita: %f reais\n", PerCapita2);
+    printf ("Super Poder: %f\n", SuperPoder2);
+    
+    //comparando o nível de poder das cartas 1 e 2
+
+    printf ("\nComparando as cartas:\n");
+    printf ("População: Carta 1 venceu (%d)\n", População1 > População2);
+    printf ("Área: Carta 1 venceu (%d)\n", Área1 > Área2);
+    printf ("PIB: Carta 1 venceu (%d)\n", PIB1 > PIB2);
+    printf ("Pontos Turísticos: Carta 1 venceu (%d)\n", Número_de_Pontos_Turísticos1 > Número_de_Pontos_Turísticos2);
+    printf ("Densidade Populacional: Carta 2 venceu (%d)\n", Densidade1 < Densidade2);
+    printf ("PIB per Capita: Carta 1 venceu (%d)\n", PerCapita1 > PerCapita2);
+    printf ("Super Poder: Carta 1 venceu (%d)\n", SuperPoder1 > SuperPoder2);
     
     return 0;
     
